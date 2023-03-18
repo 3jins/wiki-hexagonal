3bd0541386917d10a694c79760f7ecdbec950419
Sejin Jeon <jinaidy93@gmail.com>
2023-03-18 22:44:43 +0900

    Initial commit

---
+++ src/main/kotlin/org/sejin/wikihexagonal/WikiHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package org.sejin.wikihexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WikiHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<WikiHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/org/sejin/wikihexagonal/WikiHexagonalApplicationTest.kt
@@ -0,0 +1,13 @@
+package org.sejin.wikihexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WikiHexagonalApplicationTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
