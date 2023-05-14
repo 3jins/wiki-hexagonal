fb90c53375cc4c6b1331d04591ad14c23e58ea85
Sejin Jeon <jinaidy93@gmail.com>
2023-05-14 16:36:41 +0900

    Add DocumentsPage

--- frontend/src/react/App.tsx
+++ frontend/src/react/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import DocumentsPage from '@src/react/page/DocumentsPage';
+import AppContainer from '@src/react/component/organism/AppContainer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,14 +17,16 @@ export default () => {
   return (
     <QueryClientProvider client={queryClient}>
       <React.Fragment>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path='/documents'
-              element={<DocumentsPage/>}
-            />
-          </Routes>
-        </BrowserRouter>
+        <AppContainer>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/documents"
+                element={<DocumentsPage/>}
+              />
+            </Routes>
+          </BrowserRouter>
+        </AppContainer>
       </React.Fragment>
     </QueryClientProvider>
   );
---
+++ frontend/src/react/component/molecule/RenderedArticle.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.article`
+  margin: 1rem 0;
+  line-height: 1.4rem;
+  >p {
+    margin: 0.5rem 0;
+  }
+`;
